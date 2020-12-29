f1449c3d05e149505bbedec576a2b5bc1b770d89
Gonçalo Matos <gmatos.ferreira@ua.pt>
2020-12-29 16:10:16 +0000

    removed duplicate function

--- crypto_functions.py
+++ crypto_functions.py
@@ -57,77 +57,6 @@ class CryptoFunctions:
         )
 
     """
-    This method asks the server for the available protocols
-    and lets the user define the protocols to use.
-    It returns the cipher suite.
-    """
-    @staticmethod
-    def client_chosen_options(server_url):
-        # Ask server for available protocols
-        req = requests.get(f'{server_url}/api/protocols')    
-        
-        if req.status_code == 200:
-            print("Got Protocols!")
-        else:
-            print("The server is not available!")
-            exit()
-    
-        protocols = req.json()
-
-        # Cipher choice
-        while True:
-            # Show options
-            print("\nChoose a cipher algorithm: ")
-            i=1
-            for c in protocols['cipher']:
-                print(i, ")",c)
-                i+=1
-            # Receive input
-            print("> " , end =" ")
-            op = int(input())
-            if op >= 1 and op <= len(protocols['cipher']):
-                cipher = protocols['cipher'][op-1]
-                break
-            print("That is not a valid option! Try again!")
-        
-        # Digest choice
-        while True:
-            # Show options
-            print("\nChoose a digest: ")
-            i=1
-            for d in protocols['digests']:
-                print(i, ")",d)
-                i+=1
-            # Receive input
-            print("> " , end =" ")
-            op = int(input())
-            if op >= 1 and op <= len(protocols['digests']):
-                digest = protocols['digests'][op-1]
-                break
-            print("That is not a valid option! Try again!")
-
-        # Cipher mode choice
-        while True: 
-            # Show options
-            print("\nChoose a cipher mode: ")
-            i=1
-            for mode in protocols['cipher_mode']:
-                print(i, ")",mode)
-                i+=1
-            # Receive input
-            print("> " , end =" ")
-            op = int(input())
-            if op >= 1 and op <= len(protocols['cipher_mode']):
-                cipher_mode = protocols['cipher_mode'][op-1]
-                break
-            print("That is not a valid option! Try again!")
-
-        cipherSuite = {'cipher': cipher, 'digest': digest, 'cipher_mode':cipher_mode}
-        
-        return cipherSuite
-
-
-    """
     This method checks and applys a digest function to a given message
     The default size is 256
     --- Returns
@@ -165,10 +94,6 @@ class CryptoFunctions:
         mac.update(message)
         
         return mac.finalize()
-
-    #MIC é um DIGEST
-        
-        
 
     """
     This method handles symetric encryption/decription
