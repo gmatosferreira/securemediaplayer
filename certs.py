6b5761e2444ebe8d4b6c2361ca14a9034152dfed
alex <aarodrigues@ua.pt>
2020-12-29 15:38:37 +0000

    load certs

--- certs.py
+++ certs.py
@@ -1,25 +1,47 @@
 from cryptography import x509
-import PyKCS11
-import binascii
+from cryptography.hazmat.primitives.serialization import load_pem_private_key
+
 
 """
     Method used to load certificate from file 
+    return: certificate object
 """
 def load_cert(file):
     
     try:
         with open(file, "rb") as f:
-            pem_data = f.read()
-            cert = x509.load_pem_x509_certificate(pem_data)
+            data = f.read()
+            cert = x509.load_pem_x509_certificate(data)
+
         return cert
     except:
-        print("ERROR")
+        raise TypeError
 
+"""
+    Method used to load certificate key from file 
+    return: certificate key object
+"""
+def load_cert_key(file):
+    
     try:
         with open(file, "rb") as f:
-            pem_data = f.read()
-            cert = x509.load_der_x509_certificate(pem_data)
-        return cert
+            key = load_pem_private_key(f.read(), password="key".encode('utf-8'))
+        return key
     except:
-        print("ERROR")
+        raise TypeError
 
+"""
+    Method used to load certificate key from file 
+    return: certificate key object
+"""
+def load_all_certs():
+        #load server cert
+        server_cert = load_cert("certificates/server_cert.pem")
+
+        ca_cert = load_cert("certificates/CA_cert.pem" )
+        
+        rsa_private_key = load_cert_key("keys/server_cert_key.pk8")
+
+
+if __name__ == '__main__':
+    load_all_certs()
