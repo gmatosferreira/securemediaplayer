5589c3870ed512b0b9ffb8bd9eda2e0329e64853
Gonçalo Matos <gmatos.ferreira@ua.pt>
2020-12-31 10:46:30 +0000

    removed methods not used on crypto_functions

--- crypto_functions.py
+++ crypto_functions.py
@@ -26,37 +26,6 @@ class CryptoFunctions:
         return private, public
 
     """
-    This method creates a criptogram for a message ciphered with a public key
-    --- Parameteres
-    key             The key to cipher with
-    message         The text to cipher
-    algorithm       The algorithm to cipher with
-    --- Returns
-    criptogram      The text ciphered
-    """
-    @staticmethod
-    def assymetric_encryption(key, message, algorithm=hashes.SHA256):
-        return key.encrypt(
-            message,
-            padding.OAEP(
-                mgf = padding.MGF1(algorithm=hashes.SHA256()),
-                algorithm = hashes.SHA256(),
-                label = None
-            )
-        )
-
-    @staticmethod
-    def assymetric_decryption(key, cryptogram, algorithm=hashes.SHA256):
-        return key.decrypt(
-            cryptogram,
-            padding.OAEP(
-                mgf = padding.MGF1(algorithm=algorithm()),
-                algorithm = algorithm(),
-                label = None
-            )
-        )
-
-    """
     This method checks and applys a digest function to a given message
     The default size is 256
     --- Returns
@@ -78,23 +47,6 @@ class CryptoFunctions:
         
         return digest.finalize()
 
-    @staticmethod
-    def create_mac(message, key, digst_algorithm):
-        
-        hash_algorithm = None
-
-        if digst_algorithm == "SHA512":
-            hash_algorithm = hashes.SHA512()
-        elif digst_algorithm == "BLAKE2":
-            hash_algorithm = hashes.BLAKE2b(64)
-        else:
-            raise Exception("Digest Algorithm name not founded!")
-
-        mac = hmac.HMAC(key, digst_algorithm, backend=default_backend())
-        mac.update(message)
-        
-        return mac.finalize()
-
     """
     This method handles symetric encryption/decription
     --- Parameters
@@ -109,12 +61,6 @@ class CryptoFunctions:
     """
     @staticmethod
     def symetric_encryption(key,message, algorithm_name, cypher_mode, digest_mode, encode=True ):
-
-        # If key length does not match expected, create digest for it
-        print(f"\nSymetric encription with key of size {len(key)*8}...")
-        if len(key)*8 != 256:
-            key = CryptoFunctions.create_digest(key, digest_mode)
-        print(f"Symetric encription with key of size {len(key)*8}...")
 
         # Define algorithm
         algorithm = None
